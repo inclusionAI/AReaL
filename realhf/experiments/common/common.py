4a658db3dbac1f0cf6aa7ea28acba2de2d4d9ec1
晓雷 <meizhiyu.mzy@antgroup.com>
2025-04-21 15:06:17 +0800

    PullRequest: 109 [Bug Fix] Fix `nodelist` and `exclude` options for slurm scheduler.
    Merge branch mzy/exclude of git@code.alipay.com:inclusionAI/AReaL.git into main
    https://code.alipay.com/inclusionAI/AReaL/pull_requests/109
    
    Signed-off-by: 博惟 <bowei.fw@antgroup.com>
    
    
    * fix nodelist and exclude option
    * relax device mesh mapping constraints on 1 node
    * add exclude for async exp

--- realhf/experiments/common/common.py
+++ realhf/experiments/common/common.py
@@ -154,8 +154,6 @@ class CommonExperimentConfig(BaseExperimentConfig, Experiment):
             n_nodes=self.n_nodes,
             n_gpus_per_node=self.n_gpus_per_node,
             mapping=np.ones((self.n_nodes, self.n_gpus_per_node), dtype=np.int32),
-            global_mesh_name=self.nodelist,
-            name=self.nodelist,
         )
 
     def _heuristic_rpc_allocation(self) -> List[RPCAllocation]:
@@ -190,6 +188,7 @@ class CommonExperimentConfig(BaseExperimentConfig, Experiment):
                     cpu=self.cpus_per_master_worker,
                     mem=self.mem_per_master_worker,
                     nodelist=self.nodelist,
+                    exclude=self.exclude,
                 ),
             ),
             model_worker=TasksGroup(
@@ -200,6 +199,7 @@ class CommonExperimentConfig(BaseExperimentConfig, Experiment):
                     gpu_type=cluster_spec.gpu_type,
                     mem=self.mem_per_model_worker,
                     nodelist=self.nodelist,
+                    exclude=self.exclude,
                 ),
             ),
         )
@@ -351,12 +351,16 @@ class CommonExperimentConfig(BaseExperimentConfig, Experiment):
                 )
                 rpc_allocs.append(alloc)
         elif self.allocation_mode == "manual":
+            if self.nodelist is None:
+                raise ValueError(
+                    "The 'nodelist' option must be specified when using manual allocation mode."
+                )
             rpc_allocs: List[RPCAllocation] = [
                 RPCAllocation(
                     rpc=rpc,
                     device_mesh=(
                         make_device_mesh_from_name(
-                            self.global_device_mesh.name,
+                            self.nodelist,
                             self.allocations[rpc_type].device_mesh,
                             self.global_device_mesh.n_gpus_per_node,
                         )
