4ee4e062e64c4a2e0fa88435fe3eae603fa0ca6e
garrett4wade <fuwth17@gmail.com>
2025-06-01 11:04:57 +0800

    .

--- examples/env/scripts/setup-pip-deps.sh
+++ examples/env/scripts/setup-pip-deps.sh
@@ -9,12 +9,14 @@ pip install flash-attn --no-build-isolation
 
 # the sympy virtual env for reward computation
 pip install virtualenv
+rm -rf ./sympy
 python3 -m venv ./sympy
 # equivalent to install `./evaluation/latex2sympy` in the sympy virtual env
 ./sympy/bin/pip install git+https://github.com/QwenLM/Qwen2.5-Math.git#subdirectory=evaluation/latex2sympy
 ./sympy/bin/pip install regex numpy tqdm datasets python_dateutil sympy==1.12 antlr4-python3-runtime==4.11.1 word2number Pebble timeout-decorator prettytable
 
 # Install an editable sglang
+rm -rf ./sglang
 git clone -b v0.4.6.post4 https://github.com/sgl-project/sglang
 AREAL_PATH=$PWD
 cd sglang
