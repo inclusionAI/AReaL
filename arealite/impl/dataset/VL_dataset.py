109be6aae6b4d4354aa2282fea10d28740d869c0
antoinegg1 <3528942762@qq.com>
2025-07-02 20:08:35 +0800

    image_process0702_5

--- arealite/impl/dataset/VL_dataset.py
+++ arealite/impl/dataset/VL_dataset.py
@@ -4,8 +4,44 @@ import hashlib
 from PIL import Image
 from PIL.Image import Image as ImageObject
 from io import BytesIO
-import re
+import os
 import math
+
+# def process_image(
+#     image: Union[Dict[str, Any], Image.Image, str], min_pixels: Optional[int] = None, max_pixels: Optional[int] = None
+# ) -> BytesIO:
+#     # 处理不同格式的图像输入
+#     if isinstance(image, str):
+#         image = Image.open(image)
+#     elif isinstance(image, dict):
+#         image = Image.open(BytesIO(image["bytes"]))
+#     elif isinstance(image, bytes):
+#         image = Image.open(BytesIO(image))
+
+#     image.load()  # 避免 "Too many open files" 错误
+
+#     # 调整图像大小（如果超出最大像素限制，或者小于最小像素限制）
+#     if max_pixels is not None and (image.width * image.height) > max_pixels:
+#         resize_factor = math.sqrt(max_pixels / (image.width * image.height))
+#         width, height = int(image.width * resize_factor), int(image.height * resize_factor)
+#         image = image.resize((width, height))
+
+#     if min_pixels is not None and (image.width * image.height) < min_pixels:
+#         resize_factor = math.sqrt(min_pixels / (image.width * image.height))
+#         width, height = int(image.width * resize_factor), int(image.height * resize_factor)
+#         image = image.resize((width, height))
+
+#     # 如果图像模式不是 RGB，则转换为 RGB 模式
+#     if image.mode != "RGB":
+#         image = image.convert("RGB")
+
+#     # 将图像保存到 BytesIO 对象
+#     img_byte_arr = BytesIO()
+#     image.save(img_byte_arr, format="PNG")  # 你可以选择不同的格式，如PNG或JPEG
+#     img_byte_arr.seek(0)  # 将指针重置到字节流的开始位置
+
+#     return img_byte_arr
+
 
 #Standard dataset Key:"id", "image", "question", "solution", "hash"
 VL_DATASET=["clevr_count_70k", "llava_cot", "mm_mathinstruct"]
@@ -14,6 +50,7 @@ def process_VL_dataset(
     dataset: Dataset,
 ):
     if dataset.info.dataset_name.lower() == "clevr_count_70k":
+
         return process_clevr_count_dataset(dataset)
     elif dataset.info.dataset_name.lower() == "llava_cot":
         return process_llava_cot_dataset(dataset)
@@ -22,32 +59,7 @@ def process_VL_dataset(
     else:
         raise ValueError(f"Unsupported VL dataset: {dataset.info.dataset_name}. Supported datasets are: {VL_DATASET}")
 
-def process_image(
-    image: Union[Dict[str, Any], ImageObject, str], min_pixels: Optional[int]=None, max_pixels: Optional[int]=None
-) -> ImageObject:
 
-    if isinstance(image, str):
-        image = Image.open(image)
-    elif isinstance(image, dict):
-        image = Image.open(BytesIO(image["bytes"]))
-    elif isinstance(image, bytes):
-        image = Image.open(BytesIO(image))
-
-    image.load()  # avoid "Too many open files" errors
-    if max_pixels is not None and (image.width * image.height) > max_pixels:
-        resize_factor = math.sqrt(max_pixels / (image.width * image.height))
-        width, height = int(image.width * resize_factor), int(image.height * resize_factor)
-        image = image.resize((width, height))
-
-    if min_pixels is not None and (image.width * image.height) < min_pixels:
-        resize_factor = math.sqrt(min_pixels / (image.width * image.height))
-        width, height = int(image.width * resize_factor), int(image.height * resize_factor)
-        image = image.resize((width, height))
-
-    if image.mode != "RGB":
-        image = image.convert("RGB")
-
-    return image
 
 def generate_image_hash(image: Image.Image) -> str:
     img_byte_arr = BytesIO()
@@ -78,20 +90,22 @@ DatasetDict({
 def process_clevr_count_dataset(dataset: Dataset):
 
     def process_example(example):
-        processed_image=[]
+        # processed_image=[]
 
-        for image in example["images"]:
-            processed_image.append(process_image(image))
+        # for image in example["images"]:
+        #     processed_image.append(process_image(image))
         example["question"]=example["problem"]
         example["solution"]=example["answer"]
-        example["image"]=processed_image
-        example["hash"] = generate_hash(example["image"], example["question"])
+        example["image"]=example["images"]
+        # example["hash"] = generate_hash(example["image"], example["question"])
 
         return example
     dataset = dataset.map(
         lambda example: process_example(example),
-        remove_columns=["problem","answer"]
+        remove_columns=["problem","answer","images"],
+        num_proc=os.cpu_count()
     )
+
     return dataset
 
 '''
