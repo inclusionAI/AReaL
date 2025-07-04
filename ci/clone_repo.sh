59609c23b54b2c94ba967e4b8dcce8790bf22bf1
Zijian Zhang <futrime+git@outlook.com>
2025-07-04 17:05:29 +0800

    ci: fix wrong env

--- ci/clone_repo.sh
+++ ci/clone_repo.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-GIT_REPO=${GIT_REPO:?"GIT_REPO is not set"}
+GIT_REPO_URL=${GIT_REPO_URL:?"GIT_REPO_URL is not set"}
 GIT_COMMIT_SHA=${GIT_COMMIT_SHA:?"GIT_COMMIT_SHA is not set"}
 
-echo "GIT_REPO: $GIT_REPO"
+echo "GIT_REPO_URL: $GIT_REPO_URL"
 echo "GIT_COMMIT_SHA: $GIT_COMMIT_SHA"
 
 RUN_ID="areal-$GIT_COMMIT_SHA"
@@ -13,6 +13,6 @@ mkdir -p "/tmp/$RUN_ID"
 cd "/tmp/$RUN_ID"
 
 git init
-git remote add origin "https://github.bibk.top/$GIT_REPO"
+git remote add origin "$GIT_REPO_URL"
 git fetch --depth 1 origin "$GIT_COMMIT_SHA"
 git checkout FETCH_HEAD
