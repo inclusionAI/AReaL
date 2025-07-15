7a4da94641282266577d7492ca259bb5f24a74f2
chucai.dzq <chucai.dzq@alibaba-inc.com>
2025-07-15 17:30:02 +0800

    daily update

--- arealite/tests/test_trainer.py
+++ arealite/tests/test_trainer.py
@@ -18,13 +18,7 @@ from arealite.api.cli_args import GenerationHyperparameters
 from realhf.api.core.data_api import load_hf_tokenizer
 
 def main_grpo():
-    # rollout_config, training_config = load_expr_config(sys.argv[1:])
-
-    # Single-controller mode initialization
-
-
     scheduler = LocalScheduler({})
-
 
     rollout = DistributedRolloutController(
         RemoteSGLangEngine(InferenceEngineConfig(experiment_name="ff", trial_name="ff")),
@@ -36,22 +30,11 @@ def main_grpo():
         TrainControllerConfig(),
         scheduler,
     )
-    # ref = TrainController(
-    #     MegatronGRPOActor(training_config.ref),
-    #     config.training_controller_config,
-    #     scheduler,
-    # )
-    # SPMD mode initialization
-    # rollout = RemoteSGLangEngine(rollout_config.engine)
-    # actor = MegatronGRPOActor(training_config.actor)
-    # ref = MegatronGRPOActor(training_config.ref)
 
     rollout.initialize()
     print(f"rollout workers len: {len(rollout.workers)}")
     actor.initialize()
     print(f"actor workers len: {len(actor.workers)}")
-
-    # ref.initialize()
 
     # # Synchronous RL
     dataset = load_dataset("json",
@@ -92,15 +75,8 @@ def main_grpo():
 
             # input_: List[Dict[str, tensor]]
             rollout_res = rollout.rollout(batch_data, workflow=workflow)
-
             print(f"rollout_ exec success, {len(rollout_res)}")
             print(f"rollout_res: {rollout_res}")
-            # or asynchronous rollout with filtering and off-policyness control
-            # rollout_batch = rollout.prepare_batch(batch,
-            #                                       workflow=MyRolloutWorkflow(rollout_config.workflow),
-            #                                       should_accept=lambda x: x['rewards'].mean() > 0)
-
-            # print(stats)
 
 if __name__ == "__main__":
     main_grpo()
