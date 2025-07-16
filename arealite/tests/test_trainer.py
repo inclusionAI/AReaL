d242119a94130faba143945dc61d781bdd559c6c
chucai.dzq <chucai.dzq@alibaba-inc.com>
2025-07-16 15:59:01 +0800

    daily update

--- arealite/tests/test_trainer.py
+++ arealite/tests/test_trainer.py
@@ -16,8 +16,23 @@ from arealite.dataset.distributed_batch_memory import DistributedBatchMemory
 from arealite.workflow.rlvr import RLVRWorkflow
 from arealite.api.cli_args import GenerationHyperparameters
 from realhf.api.core.data_api import load_hf_tokenizer
+from arealite.api.engine_api import WeightUpdateMeta
+
+import os
+import shutil
+
+
+def clear_dir(path):
+    if os.path.exists(path):
+        for filename in os.listdir(path):
+            file_path = os.path.join(path, filename)
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
 
 def main_grpo():
+    # init controller
     scheduler = LocalScheduler({})
 
     rollout = DistributedRolloutController(
@@ -30,15 +45,13 @@ def main_grpo():
         TrainControllerConfig(),
         scheduler,
     )
-
+    # engine initialize
     rollout.initialize()
     actor.initialize()
 
-    # # Synchronous RL
     dataset = load_dataset("json",
                            data_files="/storage/xukuan.xk/repos/antnlp/personal/llm/benchmark/orz_areal_train_32.jsonl")
-    train_dataset = dataset['train']  # 取出train split
-    # train_dataset = process_rl_dataset(train_dataset)
+    train_dataset = dataset['train']
     dataloader = StatefulDataLoader(train_dataset, batch_size=1)
     batch_size = 1
     batch_data = []
@@ -47,17 +60,34 @@ def main_grpo():
     for epoch in range(epoch_num):
         data_generator = iter(dataloader)
         for step in range(step_num):
-            # get batch data
             batch_data = []
             for _ in range(batch_size):
                 batch = next(data_generator)
                 batch_data.append(batch)
 
             # Update inference engine weights
-            # wcfg = actor.upload_weights(WeightUpdateMeta)
-            # future = rollout.update_weights(wcfg)
-            # actor.upload_weights(wcfg)
-            # future.result()
+            exp_name = "ff"
+            trial_name = "ff"
+            actor_cfg = WeightUpdateMeta(
+                type="disk",
+                path=f"/storage/openpsi/checkpoint/{exp_name}/{trial_name}/",
+                alloc_mode=None,
+                comm_backend=None,
+            )
+            rollout_cfg = WeightUpdateMeta(
+                type="disk",
+                path=f"/storage/openpsi/checkpoint/{exp_name}/{trial_name}/{step}",
+                alloc_mode=None,
+                comm_backend=None,
+            )
+            '''
+            actor.upload_weights(actor_cfg)
+            print("[Trainer] actor upload_weights success.")
+            rollout.update_weights(rollout_cfg)
+            print("[Trainer] rollout update_weights success.")
+            clear_dir(rollout_cfg.path)
+            print(f"[Trainer] clear update weights dir success: {rollout_cfg.path}")
+            '''
 
             # synchronous rollout
             gconfig = GenerationHyperparameters(
@@ -73,9 +103,18 @@ def main_grpo():
 
             # input_: List[Dict[str, tensor]]
             rollout_res = rollout.rollout(batch_data, workflow=workflow)
-            print(f"rollout_ exec success, {len(rollout_res)}")
-            print(f"rollout_res: {rollout_res}")
-            torch.save(rollout_res, "rollout_res.pt")
+            print(f"[Trainer] rollout exec success, {len(rollout_res)}")
+            print(f"[Trainer] rollout_res: {rollout_res}")
+            # torch.save(rollout_res, "rollout_res.pt")
+
+            rollout_res = rollout_res.to("cpu").clone()
+            rollout_res_dict = rollout_res.to_dict()
+            for k, v in rollout_res_dict.items():
+                if isinstance(v, torch.Tensor) and v.ndim > 1 and v.shape[1] == 1:
+                    rollout_res_dict[k] = v.squeeze(1)
+            dis_batch = DistributedBatchMemory(rollout_res_dict)
+            stats = actor.train_distributed_batch(dis_batch)
+            print(f"[Trainer] train exec success, step: {step}, epoch: {epoch}, stats: {stats}")
 
 if __name__ == "__main__":
     main_grpo()
