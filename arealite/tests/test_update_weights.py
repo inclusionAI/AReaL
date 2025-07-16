fc085a3158ee040331b5b7a459af17ee43c0c8ca
chucai.dzq <chucai.dzq@alibaba-inc.com>
2025-07-16 16:08:29 +0800

    daily update

---
+++ arealite/tests/test_update_weights.py
@@ -0,0 +1,34 @@
+from arealite.api.cli_args import InferenceEngineConfig, RolloutControllerConfig
+from arealite.controller.rollout_controller import DistributedRolloutController
+from arealite.extension.asystem.remote_sglang_engine import RemoteSGLangEngine
+from arealite.scheduler.local import LocalScheduler
+from arealite.api.engine_api import WeightUpdateMeta
+
+
+def main_grpo():
+    # init controller
+    scheduler = LocalScheduler({})
+
+    rollout = DistributedRolloutController(
+        RemoteSGLangEngine(InferenceEngineConfig(experiment_name="ff", trial_name="ff")),
+        RolloutControllerConfig(),
+        scheduler,
+    )
+
+    # engine initialize
+    rollout.initialize()
+
+    # Update inference engine weights
+    rollout_cfg = WeightUpdateMeta(
+        type="disk",
+        path=f"/storage/openpsi/checkpoint/ff/ff/0",
+        alloc_mode=None,
+        comm_backend=None,
+    )
+
+    rollout.update_weights(rollout_cfg)
+    print("[Trainer] rollout update_weights success.")
+
+
+if __name__ == "__main__":
+    main_grpo()
