e9dc11283b5921b95950c2d5a5c2164412eb8309
朱晗 <lichangye.lcy@antgroup.com>
2025-07-17 16:50:56 +0800

    0717_6

--- examples/arealite/dataset/clevr_count_70k.py
+++ examples/arealite/dataset/clevr_count_70k.py
@@ -1,10 +1,43 @@
-import os
+from typing import Any, Dict,  Optional, Union
 import math
-from typing import Any, Dict, List, Optional, Union
 from PIL.Image import Image as ImageObject
+import os
 from datasets import load_dataset
 from datasets.distributed import split_dataset_by_node
+import base64
+from io import BytesIO
 
+def input_text(text:str):
+    return {"type": "input_text", "text": text}
+def input_image(base64_image: str):
+    return {"type": "input_image", "image_url":  f"data:image/jpeg;base64,{base64_image}"}
+def build_raw_message(sample: Dict[str, Any], base64_images: list[str]) -> list[Dict[str, Any]]:
+    
+    raw_message = []
+    problem_parts = [part.strip() for part in sample["problem"].split("<image>") if part.strip()]
+    insert_list = []
+    for i, part in enumerate(problem_parts):
+        if i > 0 or sample["problem"].startswith("<image>"):  
+            insert_list.append("image")
+        part = part.strip()  
+        if part:  
+            insert_list.append("text")
+    image_index = 0
+    text_index = 0
+
+    for insert_type in insert_list:
+        if insert_type == "text" and text_index < len(problem_parts):
+            raw_message.append(input_text(problem_parts[text_index].strip()))
+            text_index += 1
+        elif insert_type == "image" and image_index < len(base64_images):
+            raw_message.append(input_image(base64_images[image_index]))
+            image_index += 1
+    messages = [{"role": "user", "content": raw_message}]
+    return messages
+
+
+def encode_image(image_file):
+    return base64.b64encode(image_file).decode("utf-8")
 def convert_image(
     image: Union[Dict[str, Any], ImageObject, str], min_pixels: Optional[int], max_pixels: Optional[int]
 ) -> ImageObject:
@@ -20,9 +53,9 @@ def convert_image(
 
     if image.mode != "RGB":
         image = image.convert("RGB")
-        
-    
-    return image
+    with BytesIO() as output:
+        image.save(output, format="JPEG")
+        return output.getvalue()
 
 def get_clevr_count_70k_sft_dataset(path, split, processor, rank, world_size):
     '''
@@ -81,6 +114,20 @@ def get_clevr_count_70k_sft_dataset(path, split, processor, rank, world_size):
 
     dataset = dataset.map(lambda x: _process(x),remove_columns=["images","seq","problem"],num_proc=os.cpu_count())
     return dataset
+
+# def get_clevr_count_70k_rl_dataset(path, split,  rank, world_size):
+#     dataset = load_dataset(path=path, split=split)
+#     dataset = split_dataset_by_node(dataset, rank=rank, world_size=world_size)
+#     def process(sample):
+#         processed_images = [convert_image(image, 113*113, 336*336) for image in sample["images"]]
+#         base64_images = [encode_image(image) for image in processed_images]
+#         messages = build_raw_message(sample, base64_images)
+
+#         return {"messages": messages,"images":processed_images}
+    
+#     dataset = dataset.map(process).remove_columns(["problem"])
+#     breakpoint()
+#     return dataset
 
 def get_clevr_count_70k_rl_dataset(path, split,processor,  rank, world_size):
     dataset = load_dataset(path=path, split=split)
