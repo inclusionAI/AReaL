6e247dd2ce5d5ff1eedc45af58f11cf4b839adce
daihao <dh183333@antfin.com>
2025-07-24 22:34:15 +0800

    bugfix

--- arealite/scheduler/rpc/rpc_client.py
+++ arealite/scheduler/rpc/rpc_client.py
@@ -34,8 +34,8 @@ class RPCClient:
         return resp.status_code == 200
 
     def call_engine(self, worker_id, method, *args, **kwargs):
-        # ip, port = self._addrs[worker_id]
-        # url = f"http://{ip}:{port}/call"
+        ip, port = self._addrs[worker_id]
+        url = f"http://{ip}:{port}/call"
         # 支持变长参数
         req = (method, args, kwargs)
         print(f"call engine0: {method}, args: {args} MB, kwargs: {kwargs}",flush=True)
@@ -46,13 +46,13 @@ class RPCClient:
         mem_usage = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
         print(f"call engine2: {method}, 内存使用: {mem_usage / 1024:.2f} MB， serialized_data: {sys.getsizeof(serialized_data)}", flush=True)
 
-        # resp = requests.post(url, data=serialized_data, timeout=7200)
-        # logging.error(
-        #    f"Sent call '{method}' to {worker_id} ({ip}:{port}), status={resp.status_code}"
-        # )
-        # if resp.status_code == 200:
-        #     return cloudpickle.loads(resp.content)
-        # else:
-        #     raise RuntimeError(f"RPC call failed: {resp.content}")
+        resp = requests.post(url, data=serialized_data, timeout=7200)
+        logging.error(
+           f"Sent call '{method}' to {worker_id} ({ip}:{port}), status={resp.status_code}"
+        )
+        if resp.status_code == 200:
+            return cloudpickle.loads(resp.content)
+        else:
+            raise RuntimeError(f"RPC call failed: {resp.content}")
 
 
