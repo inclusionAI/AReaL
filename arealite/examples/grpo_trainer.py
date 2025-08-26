571eb26eb183cb0dc3b1c59fee545a3b9b244a6d
仲青 <wht499247@antgroup.com>
2025-08-26 21:53:53 +0800

    PullRequest: 719 fix
    Merge branch fix_max_tokens_config of git@code.alipay.com:inclusionAI/AReaL.git into asystem/v1.0
    https://code.alipay.com/inclusionAI/AReaL/pull_requests/719
    
    Reviewed-by: 峯回 <dh183333@antgroup.com>
    
    
    * fix

--- arealite/examples/grpo_trainer.py
+++ arealite/examples/grpo_trainer.py
@@ -42,6 +42,8 @@ def main(args):
     config, _ = load_expr_config(args, GRPOConfig)
     config: GRPOConfig
 
+    config.gconfig.max_tokens = config.gconfig.max_new_tokens + config.train_dataset.max_prompt_len
+
     if config.enable_colocate_mode:
         config.rollout.engine_config['enable_memory_saver'] = True
     
