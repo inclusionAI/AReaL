1693daa82794717bee8c10fcdd872a8eb83e9021
峯回 <dh183333@antgroup.com>
2025-09-03 16:50:54 +0800

    PullRequest: 798 update engine image to 0827-rc1
    Merge branch asystem/v1.0-0827rc1 of git@code.alipay.com:inclusionAI/AReaL.git into asystem/v1.0
    https://code.alipay.com/inclusionAI/AReaL/pull_requests/798
    
    Reviewed-by: 楚财 <chucai.dzq@alibaba-inc.com>
    
    
    * update engine image to 0827-rc1

--- arealite/controller/rollout_controller.py
+++ arealite/controller/rollout_controller.py
@@ -180,7 +180,7 @@ class DistributedRolloutController(RolloutController):
         )
         engineSpec.env_vars["ENGINE_PACKAGE_PATH"] = engine_path
         engineSpec.env_vars["WORKER_IMAGE"] = (
-            "/storage/openpsi/images/hybrid-engine-13570177-20250829175427.sif"
+            "/storage/openpsi/images/hybrid-engine-13680179-20250902202456.sif"
         )
         engineSpec.env_vars["WORKER_LOG_DIR"] = (
             "/storage/openpsi/experiments/logs/root/{experiment_name}/{trial_name}".format(
