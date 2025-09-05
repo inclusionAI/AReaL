7fed17f620f60c91ec9edfe91c02e131f130330b
步偶 <sam.gjx@antgroup.com>
2025-09-05 20:35:08 +0800

    delete unused import

--- examples/countdown/train.py
+++ examples/countdown/train.py
@@ -3,14 +3,11 @@ import os
 import sys
 import uuid
 import itertools
-import json
-import gc
 import aiofiles
 import aiofiles.os
 import colorama
 import torch
 import torch.distributed as dist
-import numpy as np
 from copy import deepcopy
 from datasets import load_dataset
 from datasets.distributed import split_dataset_by_node
@@ -20,10 +17,6 @@ from transformers import PreTrainedTokenizerFast
 from areal.utils.evaluator import Evaluator
 from areal.utils.hf_utils import load_hf_tokenizer
 from areal.utils.recover import RecoverHandler
-from dataclasses import dataclass, field
-from typing import List
-
-import hashlib
 
 from areal.api.cli_args import (
     GenerationHyperparameters,
@@ -40,7 +33,6 @@ from areal.api.io_struct import (
 from areal.api.workflow_api import RolloutWorkflow
 from areal.api.cli_args import GRPOConfig
 from areal.api.engine_api import InferenceEngine
-from areal.dataset import get_custom_dataset
 from areal.engine.ppo.actor import FSDPPPOActor
 from areal.engine.sglang_remote import RemoteSGLangEngine
 from areal.utils.data import concat_padded_tensors
