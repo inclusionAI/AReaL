4b8da72d497858ba4a901923397b093ac2ff8a2e
garrett4wade <fuwth17@gmail.com>
2025-09-13 20:55:20 +0800

    .

--- areal/utils/fsdp.py
+++ areal/utils/fsdp.py
@@ -80,7 +80,7 @@ __all__ = [
 logger = logging.getLogger("FSDPEngine")
 
 
-# Copied from torchtiton.
+# Copied from torchtitan. Used for Qwen3 Q/K norm.
 # NOTE: This is to achieve replicate computation on the gate module in the MoE router.
 # It does nothing other than (1) setting the module parameters as DTensors on the given mesh
 # and (2) inserting hooks to module boundary to change torch.Tensor to DTensor and back.
