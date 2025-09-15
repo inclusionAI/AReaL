8d164dab8b4a2951ad66605820731e693fa05c7c
Wentai Zhang <zhangwentai.zwt@antgroup.com>
2025-09-15 17:38:27 +0800

    Fix the bug of embeds_token for VL models

--- areal/engine/fsdp_engine.py
+++ areal/engine/fsdp_engine.py
@@ -201,8 +201,8 @@ class FSDPEngine(BaseHFEngine):
             "layers.*.self_attn.k_proj": ColwiseParallel(),
             "layers.*.self_attn.v_proj": ColwiseParallel(),
             # special q/k norm for qwen3
-            "layers.*.self_attn.k_norm": NoParallel(),
             "layers.*.self_attn.q_norm": NoParallel(),
+            "layers.*.self_attn.k_norm": NoParallel(),
             # Reduce in RowwiseParallel, Scatter by Shard(1)
             "layers.*.self_attn.o_proj": RowwiseParallel(
                 output_layouts=Shard(1),
@@ -234,6 +234,15 @@ class FSDPEngine(BaseHFEngine):
                 )
 
             if isinstance(self.model.model.language_model, nn.Module):
+                # For vision-language models, the embedding layer is not sharded
+                # since the visual part uses it but without proper tensor parallelism.
+                # Meanwhile, the first transformer layer needs to shard the input.
+                model_tp_plan.pop("embed_tokens", None)
+                model_tp_plan["layers.0"] = PrepareModuleInput(
+                    input_layouts=Replicate(),
+                    desired_input_layouts=Shard(1),
+                )
+
                 parallelize_module(
                     self.model.model.language_model,
                     device_mesh=device_mesh,
