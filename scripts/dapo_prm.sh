d827ce8f9c425c62dcf82c34fc8dc6f4f370905c
AAA <3225329724@qq.com>
2025-10-02 11:39:17 +0800

    update

--- scripts/dapo_prm.sh
+++ scripts/dapo_prm.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env bash
 set -euo pipefail
-export CUDA_VISIBLE_DEVICES=0,1,2
+export CUDA_VISIBLE_DEVICES=0,1
 N_GPU=2
-EXP_NAME=gsm8k-dapo-prm
+EXP_NAME=greso-dapo-clip-delta
 TRIAL_NAME=trial0
-FILE_ROOT=/data/yl/AReaL/tmp/areal/experiments
-ACTOR_PATH=/data/yl/model/Qwen/Qwen2.5-1.5B-Instruct
-PRM_PATH=/data/yl/model/Qwen/Qwen2.5-Math-PRM-7B
+FILE_ROOT=/data/yanglu/AReaL/tmp/areal/experiments
+ACTOR_PATH=/data/yanglu/model/Qwen/Qwen2.5-Math-7B
+PRM_PATH=/data/yanglu/model/Qwen/Qwen2.5-Math-PRM-7B
+TRAIN_DATASET_PATH=/data/yanglu/dataset/greso
+VALID_DATASET_PATH=/data/yanglu/dataset/greso
 
 TOTAL_TRAIN_EPOCHS=1
 
 python3 -m areal.launcher.local \
-    examples/prm/gsm8k_dapo_prm.py \
+    examples/prm/greso_dapo_prm.py \
     --config examples/experimental/dapo/gsm8k_dapo.yaml \
     experiment_name="$EXP_NAME" \
     trial_name="$TRIAL_NAME" \
@@ -22,7 +24,12 @@ python3 -m areal.launcher.local \
     cluster.n_nodes=1 \
     cluster.n_gpus_per_node="$N_GPU" \
     cluster.fileroot="$FILE_ROOT" \
+    +gconfig.top_p=0.7 \
     actor.path="$ACTOR_PATH" \
     actor.optimizer.lr=1e-6 \
-    actor.optimizer.weight_decay=0.1 \
-    actor.overlong_reward_penalty=false 
+    actor.optimizer.weight_decay=0.01 \
+    actor.overlong_reward_penalty=false \
+    actor.ppo_n_minibatches=64 \
+    +actor.c_clip=10.0 \
+    train_dataset.path="$TRAIN_DATASET_PATH" \
+    valid_dataset.path="$VALID_DATASET_PATH"
