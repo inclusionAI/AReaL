35974dbab11bccb574b8a46b9ff4141375f49805
garrett4wade <fuwth17@gmail.com>
2025-10-13 16:07:04 +0800

    refactor: merge vllm and sglang engine implementation

---
+++ areal/core/__init__.py
@@ -0,0 +1,11 @@
+"""Core AREAL components."""
+
+from areal.core.remote_inf_engine import (
+    RemoteInfBackendProtocol,
+    RemoteInfEngine,
+)
+
+__all__ = [
+    "RemoteInfBackendProtocol",
+    "RemoteInfEngine",
+]
