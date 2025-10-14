54e0815bd998613fd9b9aed60043c5981292e2b6
nuzant <mzy9819@gmail.com>
2025-10-14 20:03:32 +0800

    tested

--- run_local.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-python3 -m areal.launcher.local examples/math/gsm8k_grpo.py --config examples/math/gsm8k_grpo.yaml experiment_name=gsm8k-grpo trial_name=test0 \
-    allocation_mode=sglang.d1+d1 cluster.n_gpus_per_node=2
