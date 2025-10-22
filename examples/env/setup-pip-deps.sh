9d7a8a1c0344836f5b3475163557b8ebf278c344
nuzant <mzy9819@gmail.com>
2025-10-22 17:32:06 +0800

    .

--- examples/env/setup-pip-deps.sh
+++ examples/env/setup-pip-deps.sh
@@ -4,12 +4,12 @@ pip install uv
 uv pip install -U pip
 uv pip uninstall pynvml cugraph-dgl dask-cuda cugraph-service-server raft-dask cugraph cuml cugraph-pyg
 uv pip install torch==2.8.0 torchaudio torchvision "deepspeed>=0.17.2" pynvml
-uv pip install flashinfer-python==0.3.1 --no-build-isolation
+pip install flashinfer-python==0.3.1 --no-build-isolation
 uv pip install "sglang[all]==0.5.2" --prerelease=allow
-uv pip install megatron-core==0.13.1 nvidia-ml-py
+pip install megatron-core==0.13.1 nvidia-ml-py
 # NOTE: To use megatron training backend with transformers engine, 
 # you need to install flash-attn<=2.8.1, which requires compilation with torch==2.8.0.
-uv pip install "flash-attn==2.8.3" --no-build-isolation
+pip install "flash-attn==2.8.3" --no-build-isolation
 uv pip install vllm==0.10.2
 
 # Package used for calculating math reward
