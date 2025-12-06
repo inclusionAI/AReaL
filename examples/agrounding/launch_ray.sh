d5048851b7a6552c7c6f79f45400488c30520f1d
lichangye.lcy <lichangye.lcy@antgroup.com>
2025-12-06 15:38:02 +0800

    1206

--- examples/agrounding/launch_ray.sh
+++ examples/agrounding/launch_ray.sh
@@ -1,5 +1,4 @@
-# 0) 基础环境（容器里）
-export HOST_IP="33.180.163.80"
+export HOST_IP=""
 export RAY_PORT=26379                       
 export NCCL_IB_DISABLE=1
 export NCCL_DEBUG=WARN
@@ -7,7 +6,6 @@ export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 export WANDB_BASE_URL=""
 export WANDB_API_KEY=""
 
-# 1) 提高系统限制 & 清理残留
 ulimit -n 65536
 ulimit -u unlimited
 ray stop || true
@@ -16,7 +14,6 @@ rm -rf /tmp/ray
 
 export RAY_raylet_start_wait_time_s=60
 
-# 3) 启动 head
 ray start --head \
   --node-ip-address="$HOST_IP" \
   --port="$RAY_PORT" \
