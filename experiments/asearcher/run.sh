0705ea86cdeac7c9d268ad644c1f49f6643ceeb7
meizhiyu.mzy <meizhiyu.mzy@antgroup.com>
2026-01-16 15:10:02 +0800

    .

--- experiments/asearcher/run.sh
+++ experiments/asearcher/run.sh
@@ -8,12 +8,13 @@ rsync -av --delete --exclude='.git' --exclude='*.pyc' /home/admin/meizhiyu.mzy/i
 
 # Change enable_tree_training and pad_to_maximum to False in packed seq baseline
 # Change user_llm_base_url to actual server address
-srun --mpi=pmi2 -J mzy-tau2-server --chdir /storage/openpsi/users/meizhiyu.mzy/workspace/$folder_name/AReaL --cpus-per-task=128 --gres=gpu:8 --mem-per-cpu=12G --pty \
+srun --mpi=pmi2 -J mzy-asearcher --chdir /storage/openpsi/users/meizhiyu.mzy/workspace/$folder_name/AReaL --cpus-per-task=128 --gres=gpu:8 --mem-per-cpu=12G --pty \
     singularity exec --no-home --writable-tmpfs --nv --pid --bind /storage:/storage \
     --env PYTHONPATH=/storage/openpsi/users/meizhiyu.mzy/workspace/$folder_name/AReaL \
     --env WANDB_BASE_URL=http://8.150.1.98:8080 \
     --env WANDB_API_KEY=local-8726acfd20a41b5ab8ac35fd51014b15169fa20a \
     --env HF_ENDPOINT=https://hf-mirror.com \
+    --env RAG_SERVER_ADDR_DIR=/storage/openpsi/users/wanghuaijie.whj/jwhj/ASearcher/rag_addr \
     /storage/openpsi/images/areal-latest.sif \
     bash -c "python3 -m areal.launcher.local examples/search_agent/asearcher/train.py \
         --config experiments/asearcher/config.yaml \
@@ -26,7 +27,7 @@ srun --mpi=pmi2 -J mzy-tau2-server --chdir /storage/openpsi/users/meizhiyu.mzy/w
         cluster.n_nodes=1 \
         cluster.n_gpus_per_node=8 \
         allocation_mode=sglang:d6+d2 \
-        actor.path=/storage/openpsi/models/Qwen__Qwen2.5-1.5B/ \
+        actor.path=/storage/openpsi/models/Qwen__Qwen2-1.5B-Instruct/ \
         train_dataset.path=/storage/openpsi/users/meizhiyu.mzy/mydata/asearcher/ASearcher-LRM-35k.jsonl \
         judge_engine.experiment_name=mzy-asearcher-qwen72b-inst-server-only \
         judge_engine.trial_name=run0 \
--- experiments/asearcher/run_server.sh
+++ experiments/asearcher/run_server.sh
@@ -5,5 +5,9 @@ rsync -av --delete --exclude='.git' --exclude='*pyc' /home/admin/meizhiyu.mzy/in
 cd /storage/openpsi/users/meizhiyu.mzy/workspace/$folder_name/AReaL && \
     python3 -m areal.launcher.slurm examples/search_agent/asearcher/train.py \
         --config experiments/asearcher/config.yaml \
+        cluster.n_nodes=1 \
+        cluster.n_gpus_per_node=8 \
+        cluster.fileroot=/storage/openpsi/users/meizhiyu.mzy/experiments \
+        cluster.name_resolve.nfs_record_root=/storage/openpsi/users/meizhiyu.mzy/experiments/name_resolve \
         experiment_name=mzy-asearcher-qwen72b-inst-server-only \
-        trial_name=run0 cluster.n_nodes=1 allocation_mode=sglang.d2t4p1 actor.path=/storage/openpsi/models/Qwen__Qwen2.5-72B-Instruct 
+        trial_name=run0 cluster.n_nodes=1 allocation_mode=sglang:d2t4 actor.path=/storage/openpsi/models/Qwen__Qwen2.5-72B-Instruct 
