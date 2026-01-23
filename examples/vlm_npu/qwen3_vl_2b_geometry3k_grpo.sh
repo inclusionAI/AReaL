fd84fa4a03132d714782e2975db1ae798b469b39
Huawei Vancouver ICI Lab <zhenan.fan1@huawei.com>
2026-01-23 10:03:17 -0800

    Add qwen3-vl-dense support and example

--- examples/vlm_npu/qwen3_vl_2b_geometry3k_grpo.sh
+++ examples/vlm_npu/qwen3_vl_2b_geometry3k_grpo.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+export USE_OPTIMIZED_MODEL=0
 
 python -m areal.launcher.local \
-    examples/vlm_npu/qwen3_vl_2b_geometry3k_grpo.py --config examples/vlm_npu/qwen3_vl_2b_geometry3k_grpo.yaml
+    examples/vlm_npu/geometry3k_grpo.py --config examples/vlm_npu/qwen3_vl_2b_geometry3k_grpo.yaml
