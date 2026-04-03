5e5875f8f2657235b37672addb951931f1d72e75
lichangye.lcy <lichangye.lcy@antgroup.com>
2026-04-03 15:38:32 +0800

    0403_remove_verl_agent

--- geo_edit/verl-agent/agent_system/environments/env_package/geo_edit/__init__.py
+++
@@ -1,7 +0,0 @@
-"""GeoEdit environment package for verl-agent."""
-
-from agent_system.environments.env_package.geo_edit.envs import (
-    GeoEditMultiProcessEnv,
-    build_geo_edit_envs,
-)
-from agent_system.environments.env_package.geo_edit.reward import compute_reward
