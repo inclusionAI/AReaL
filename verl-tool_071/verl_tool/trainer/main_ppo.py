c35577aff5df6ee7ce41804289a91bab68c66521
lichangye.lcy <lichangye.lcy@antgroup.com>
2026-04-07 20:45:51 +0800

    up_verl_debug_1

--- verl-tool_071/verl_tool/trainer/main_ppo.py
+++ verl-tool_071/verl_tool/trainer/main_ppo.py
@@ -265,7 +265,7 @@ class TaskRunner:
         # validate config
         validate_config(
             config=config,
-            use_reference_policy=need_reference_policy(self.role_worker_mapping),
+            use_reference_policy=need_reference_policy(config),
             use_critic=need_critic(config),
         )
 
