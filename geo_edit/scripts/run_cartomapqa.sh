a016f5a2043f1f5ae33c0324c7785f1f365d8f87
lichangye.lcy <lichangye.lcy@antgroup.com>
2026-04-07 22:18:03 +0800

    0407

--- geo_edit/scripts/run_cartomapqa.sh
+++ geo_edit/scripts/run_cartomapqa.sh
@@ -20,9 +20,9 @@ SUBSETS=(
     # "MFS:cartomapqa_mfs"
     # "MML:cartomapqa_mml"
     # "MTMF:cartomapqa_mtmf" #需要结构化judge
-    # "RLE:cartomapqa_rle"
+    "RLE:cartomapqa_rle"
     # "SRN:cartomapqa_srn"
-    "STMF_counting:cartomapqa_stmf_counting"
+    # "STMF_counting:cartomapqa_stmf_counting"
     # "STMF_name_listing:cartomapqa_stmf_name_listing"
     # "STMF_presence:cartomapqa_stmf_presence"
 )
