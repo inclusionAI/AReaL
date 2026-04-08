37df71bc41e24ca10ac2c2ccc80d7a307c7c874e
lichangye.lcy <lichangye.lcy@antgroup.com>
2026-04-08 12:25:04 +0800

    verl_071_image

--- verl-tool_071/examples/train/geo_edit/setup_env.sh
+++ verl-tool_071/examples/train/geo_edit/setup_env.sh
@@ -1,3 +1,4 @@
 pip3 config set global.index-url https://pypi.antfin-inc.com/simple && pip3 config set global.extra-index-url "" && pip install -U pip
 cd verl-tool_071/verl
 pip3 install -e ".[vllm]"
+pip install fire iopath ftfy tensordict codetiming qwen_omni_utils nvitop httptools colorlog
