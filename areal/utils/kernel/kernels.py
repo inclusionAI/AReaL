0b044e514ab77a07b50b73258fc323e778697ea4
bingyechen <bingyechen@bytedance.com>
2026-05-10 12:19:57 +0800

    docs(kernels): fix

--- areal/utils/kernel/kernels.py
+++ areal/utils/kernel/kernels.py
@@ -1,36 +1,7 @@
-#
-# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
-# SPDX-License-Identifier: Apache-2.0
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
-# Copyright 2024 Bytedance Ltd. and/or its affiliates
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 """
 Implementations of the linear cross entropy with token entropy kernel.
 
+Ref some code from verl.
 The Triton kernel implementations fuse the matmul with cross-entropy
 reduction so that the ``[num_tokens, vocab_size]`` logits tensor is never
 materialized, trading kernel-launch overhead for large memory savings.
